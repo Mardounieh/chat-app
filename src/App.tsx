ac11d09e7bf4faa310e24b655537702359e05a61
mardounieh <amir.m.hosseini.8.0@gmail.com>
2026-05-08 23:35:14 +0330

    i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { useTranslation } from "react-i18next";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    const dir = i18n.language === "fa" ? "rtl" : "ltr";
+    document.documentElement.dir = dir;
+    document.documentElement.lang = i18n.language;
+  }, [i18n.language]);
+
   return <div></div>;
 }
 
